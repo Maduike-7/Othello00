e0697d06dc36c1058dcb7b755967824d47089121
Nic1493 <nic.choy107@gmail.com>
2019-12-23 01:08:47 -0500

    Update Game.cs
    - fixed bug where .exe build would not reset static variables upon starting a new game

--- Assets/Scripts/Game/Game.cs
+++ Assets/Scripts/Game/Game.cs
@@ -41,8 +41,12 @@ public class Game : MonoBehaviour
 
     void Awake()
     {
-        ResetGameState();
         InitGameBoard();
+    }
+
+    void Start()
+    {
+        ResetGameState();
     }
 
     void ResetGameState()
