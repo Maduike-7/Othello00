6a8b7f928d85de06df0d1bd47571d43307381559
Nic1493 <nic.choy107@gmail.com>
2021-12-20 22:45:24 -0500

    Menu updates
    - added linebreak.png
    - updated Options menu layout
    - restructured _Imported folder
    - removed coroutine to transition between backgrounds in BackgroundController.cs

--- Assets/Scripts/Menu/BackgroundController.cs
+++ Assets/Scripts/Menu/BackgroundController.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-using static CoroutineHelper;
 
 public class BackgroundController : MonoBehaviour
 {
@@ -11,13 +9,7 @@ public class BackgroundController : MonoBehaviour
     [Space]
 
     [SerializeField] Image currentBackground;
-    [SerializeField] Image nextBackground;
-
-    [Space]
-
     [SerializeField] List<Image> backgroundImages;
-
-    IEnumerator backgroundFadeCoroutine;
 
     void Awake()
     {
@@ -26,43 +18,6 @@ public class BackgroundController : MonoBehaviour
 
     void OnChangeBackgroundImage()
     {
-        if (backgroundFadeCoroutine != null)
-        {
-            StopCoroutine(backgroundFadeCoroutine);
-        }
-
-        backgroundFadeCoroutine = FadeBackgroundImage();
-        StartCoroutine(backgroundFadeCoroutine);
-    }
-
-    IEnumerator FadeBackgroundImage()
-    {
-        float currentTime = 0f, totalLerpTime = 1f;
-
-        currentBackground.sprite = nextBackground.sprite;
-
-        nextBackground.enabled = true;
-        nextBackground.sprite = backgroundImages[(int)userSettings.backgroundImage].sprite;
-
-        while (currentTime < totalLerpTime)
-        {
-            float alpha = currentTime / totalLerpTime;
-
-            Color currentBackgroundColour = currentBackground.color;
-            currentBackgroundColour.a = 1f - alpha;
-            currentBackground.color = currentBackgroundColour;
-
-            Color nextBackgroundColour = nextBackground.color;
-            nextBackgroundColour.a = alpha;
-            nextBackground.color = nextBackgroundColour;
-
-            currentTime += Time.deltaTime;
-            yield return EndOfFrame;
-        }
-
-        currentBackground.sprite = nextBackground.sprite;
-        currentBackground.color = Color.white;
-
-        nextBackground.enabled = false;
+        currentBackground.sprite = backgroundImages[(int)userSettings.backgroundImage].sprite;
     }
 }
