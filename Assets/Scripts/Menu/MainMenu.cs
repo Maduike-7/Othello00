966a069fbca2945d730042742847fc1f10760bec
Nic1493 <nic.choy107@gmail.com>
2022-01-01 16:02:43 -0500

    Update project settings
    changed game name to "Reversi" (updated Main Menu + ProjectSettings accordingly)

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -2,9 +2,11 @@
 
 public class MainMenu : Menu
 {
+#if UNITY_ANDROID
     public override void HandleBackButtonInput()
     {
         AndroidJavaObject androidJavaObject = new AndroidJavaClass("com.unity3d.player.UnityPlayer").GetStatic<AndroidJavaObject>("currentActivity");
         androidJavaObject.Call<bool>("moveTaskToBack", true);
     }
+#endif
 }
